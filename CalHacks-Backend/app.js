04b7f5c5aa8819d264e2d27c0f55f7bc5b4b679f
manu183 <manuellang183@gmail.com>
2018-11-03 13:52:31 -0700

    more routes

--- CalHacks-Backend/app.js
+++ CalHacks-Backend/app.js
@@ -4,45 +4,11 @@ require('dotenv').config()
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 
-// get all information for a certain patient
-app.get('/patients/:id', function(req, res) {
+app.get('/faq', function(req, res) {
     MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
-        const collection = client.db("calhacks-alzheimer").collection("patients");
-        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
-            res.send(result);
-        });
-        client.close();
-     });
-});
-
-// get all questions for a certain patient
-app.get('/patients/:id/questions', function(req, res) {
-    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
-        const collection = client.db("calhacks-alzheimer").collection("patients");
-        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
-            res.send(result["questions"]);
-        });
-        client.close();
-     });
-});
-
-// get all recommendations for a certain patient
-app.get('/patients/:id/recommendations', function(req, res) {
-    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
-        const collection = client.db("calhacks-alzheimer").collection("patients");
-        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
-            res.send(result);
-        });
-        client.close();
-     });
-});
-
-// get all history for a certain patient
-app.get('/patients/:id/history', function(req, res) {
-    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
-        const collection = client.db("calhacks-alzheimer").collection("patients");
-        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
-            res.send(result["history"]);
+        const collection = client.db("calhacks-alzheimer").collection("faqs");
+        collection.find({}).toArray(function(err,arr){
+            res.send(arr);
         });
         client.close();
      });
@@ -54,6 +20,54 @@ app.get('/patients', function(req, res) {
         const collection = client.db("calhacks-alzheimer").collection("patients");
         collection.find({}).toArray(function(err,arr){
             res.send(arr);
+        });
+        client.close();
+     });
+});
+
+// get all information for a certain patient
+app.get('/patients/:id', function(req, res) {
+    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
+        const collection = client.db("calhacks-alzheimer").collection("patients");
+        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
+            if(err) res.send("Not found.");
+            res.send(result);
+        });
+        client.close();
+     });
+});
+
+// get all questions for a certain patient
+app.get('/patients/:id/questions', function(req, res) {
+    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
+        const collection = client.db("calhacks-alzheimer").collection("patients");
+        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
+            if(err) res.send("Not found.");
+            res.send(result["questions"]);
+        });
+        client.close();
+     });
+});
+
+// get all recommendations for a certain patient
+app.get('/patients/:id/recommendations', function(req, res) {
+    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
+        const collection = client.db("calhacks-alzheimer").collection("patients");
+        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
+            if(err) res.send("Not found.");
+            res.send(result);
+        });
+        client.close();
+     });
+});
+
+// get all history for a certain patient
+app.get('/patients/:id/history', function(req, res) {
+    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
+        const collection = client.db("calhacks-alzheimer").collection("patients");
+        collection.findOne({_id: new ObjectId(req.params.id)}, function(err,result){
+            if(err) res.send("Not found.");
+            res.send(result["history"]);
         });
         client.close();
      });
@@ -94,6 +108,31 @@ app.post('/patients/:id/recommendations/:value', function(req, res) {
             if ( err ) throw err;
             res.send("Done");
         });
+        client.close();
+     });
+});
+
+app.get('/recommendations/:value', function(req, res) {
+    MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true },  function(err, client) {
+        const collection = client.db("calhacks-alzheimer").collection("recommendations");
+        if(req.params.value < 3)
+        {
+            collection.find({stadium: "late"}).toArray(function(err,arr){
+                res.send(arr);
+            });
+        }
+        else if(req.params.value < 5)
+        {
+            collection.find({stadium: "middle"}).toArray(function(err,arr){
+                res.send(arr);
+            });
+        }
+        else
+        {
+            collection.find({stadium: "early"}).toArray(function(err,arr){
+                res.send(arr);
+            });      
+        }
         client.close();
      });
 });
